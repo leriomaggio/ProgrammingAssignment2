a7feeb10429f5d5add50d8fc544a1b24979341fc
Valerio Maggio <valeriomaggio@gmail.com>
2014-09-21 09:27:18 +0200

    First version of the code for the Assignment: so far there is room for a plenty of optimizations! (this means that this commit will not be considered for submission).
    The version of the code should be correct&running. However, this is
    the list of TODO:
    
    * Make the code robust to error conditions
        - e.g., check whether or not the input data matrix is invertible
    * Test (RUnit)
    * Add documentation and comment

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,33 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    ## This function create a special `matrix` objects that is able to 
+    ## store the value of the inverse (computed by the `solve()` function)
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL  # everytime matrix data changes, flush the cache!
+    }
+    get <- function() x
+    setInverse <- function(inverse) inv <<- inverse
+    getInverse <- function() inv
+    list(set = set, get = get,
+         setinverse = setInverse,
+         getinverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinverse()
+    if(!is.null(inv)) {
+        message("getting cached data")  # Non-fatal condition
+        return(inv)  # return the cached inverse
+    }
+    data <- x$get()
+    inv <- solve(data)  # we need to check if the matrix is invertible
+    x$setinverse(inv)
+    inv
 }
